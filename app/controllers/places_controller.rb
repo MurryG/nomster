c9fc5a73e847a5086c7766aadf1942145891ed14
MurryG <mercedes.gasser@uwalumni.com>
2019-10-04 18:34:11 +0000

    Added geocoding feature

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,9 +14,9 @@ include Pagy::Backend
 
 
   def create
-    current_user.places.create(place_params)
+    @place = current_user.places.create(place_params)
     if @place.valid?
-      redirect_to root_path
+     redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
